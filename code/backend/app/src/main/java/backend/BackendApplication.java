95d089c181333e2cb015de5890066b3cd0c256d6
Lawrence0510 <lawrence0510@gmail.com>
2023-05-09 18:00:33 +0800

    基本架構完成

--- code/backend/app/src/main/java/backend/BackendApplication.java
+++
@@ -1,9 +0,0 @@
-package backend;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-public class BackendApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
-    }
-}
