992c7af840c0978677baaf38c25494767e617d66
Lawrence0510 <lawrence0510@gmail.com>
2023-05-12 03:21:31 +0800

    把範例html/css貼上來貼上來轉成Vue(還沒成功)

--- code/frontend/src/App.vue
+++ code/frontend/src/App.vue
@@ -1,19 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TestComponent msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TestComponent from "./components/TestComponent.vue"
 
 export default {
-  name: 'App',
+  name: 'TestComponent',
   components: {
-    HelloWorld
+    TestComponent
   }
 }
 </script>
-
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
