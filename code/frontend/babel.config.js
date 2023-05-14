860dcbdc3e87a99be3034b8a181dd4b6103c31f3
Lawrence0510 <lawrence0510@gmail.com>
2023-05-14 11:32:23 +0800

    Bug fixed

--- code/frontend/babel.config.js
+++ code/frontend/babel.config.js
@@ -1,11 +1,5 @@
 module.exports = {
   presets: [
-    '@babel/preset-env',
     '@vue/cli-plugin-babel/preset'
-  ],
-  plugins: [
-    '@babel/plugin-transform-runtime',
-    'transform-vue-template',
-    'transform-es2015-modules-commonjs'
   ]
 }
