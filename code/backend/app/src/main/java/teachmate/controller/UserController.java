362bb3fd722f150fa95c1e7e7706f352b3ceae7e
Lawrence0510 <lawrence0510@gmail.com>
2023-05-14 16:07:01 +0800

    springboot尚未成功執行(註冊功能)

--- code/backend/app/src/main/java/teachmate/controller/UserController.java
+++ code/backend/app/src/main/java/teachmate/controller/UserController.java
@@ -1,34 +1,21 @@
 package teachmate.controller;
 
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import teachmate.model.User;
 import teachmate.service.UserService;
 
-@RestController
-@RequestMapping("/api")
+//在 controller 中建立一個 POST 請求的 API，並綁定到一個註冊的方法。
 public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody Map<String, String> requestBody) {
-        String email = requestBody.get("email");
-        String password = requestBody.get("password");
-
-        boolean isValidCredentials = userService.verifyUserCredentials(email, password);
-
-        if (isValidCredentials) {
-            return ResponseEntity.ok("Login successful");
-        } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid email or password");
-        }
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        User registeredUser = userService.registerUser(user);
+        return new ResponseEntity<>(registeredUser, HttpStatus.CREATED);
     }
 }
